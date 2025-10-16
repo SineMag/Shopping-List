5ee902bb0c06e0ee565295c148ed4a915806e009
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-10-16 11:10:52 +0200

    feat: add server.ts

---
+++ src/controller/lists.ts
@@ -0,0 +1,26 @@
+import { ShoppingItem } from '../types/lists';
+
+let items: ShoppingItem[] = []
+
+let currentId = 0;
+
+export const getItems = (): ShoppingItem[] => {
+    return items;
+}
+
+export const getItemById = (id: number): ShoppingItem | undefined => {
+    const item = items.find(item => item.id === id);
+
+    return item;
+}
+
+export const addItem = (name: string, quantity: number, category: string): ShoppingItem => {
+    const newItem: ShoppingItem = {
+        id: currentId++,
+        name,
+        quantity,
+        category
+    }
+    items.push(newItem);
+    return newItem;
+}
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http, { IncomingMessage, ServerResponse } from "http";
+import { itemsRoute } from "./routes/lists";
+
+const PORT = 4000;
+
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+  console.log(req.url, "url");
+
+  if (req.url?.startsWith("/items")) {
+    itemsRoute(req, res);
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Route Not Found" }));
+  }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
